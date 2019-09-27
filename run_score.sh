97e8cf6ea583ee9686962d17a98f58f58499785f
alisterl <z6liao@uwaterloo.ca>
2019-09-26 23:21:48 -0400

    change results folder to networks

--- run_score.sh
+++ run_score.sh
@@ -10,20 +10,20 @@ e.g., ./run_score.sh wine.BIC.3 for ./scores/wine.BIC.3 and a Bayes factor of 3.
 exit 1
 fi
 
-mkdir -p ./results/settings
+mkdir -p ./networks/settings
 
 echo "#GOBNILP parameters for finding the optimal network
-gobnilp/outputfile/solution = \"./results/$1.opt\"" > ./results/settings/$1.opt
+gobnilp/outputfile/solution = \"./networks/$1.opt\"" > ./networks/settings/$1.opt
 
-./gobnilp/bin/gobnilp -f=jkl -g=./results/settings/$1.opt ./scores/$1
+./gobnilp/bin/gobnilp -f=jkl -g=./networks/settings/$1.opt ./scores/$1
 
-score=$(tail -1 ./results/$1.opt | awk '{print $4}')
+score=$(tail -1 ./networks/$1.opt | awk '{print $4}')
 
 echo "#GOBNILP parameters for collecting networks
 gobnilp/countsols = TRUE
 gobnilp/countsols/collect = TRUE
 gobnilp/countsols/sollimit = 150000
 gobnilp/objlimit = $(echo "$score-l($2)" | bc -l)
-gobnilp/outputfile/countsols = \"./results/$1\"" > ./results/settings/$1
+gobnilp/outputfile/countsols = \"./networks/$1\"" > ./networks/settings/$1
 
-./gobnilp/bin/gobnilp -g=./results/settings/$1 -f=jkl ./scores/$1
+./gobnilp/bin/gobnilp -g=./networks/settings/$1 -f=jkl ./scores/$1
